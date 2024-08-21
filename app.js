095ccd59412806bb4261f64c079a04cb70979fa5
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-21 19:38:01 +0330

    private properties

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-class Triangle {
-  edgeNum = 3;
-  //   different than static properties like those are tied to class,
-  // but these are public properties which are tied to instances
-  //   can access to it with this on instance
-  //   can just set a property in constructor like this.edgeNum = 3
-  // but this avoides repetition( clean up our constructor)
-
-  constructor(a, b) {
-    this._a = a;
-    this._b = b;
+class Circle {
+  #radius;
+  constructor(r) {
+    this.#radius = r;
+  }
+  get radius() {
+    return this.#radius;
   }
 }
 
-const tri = new Triangle(3, 4);
-console.log(tri.edgeNum);
+const c = new Circle(10);
+console.log(c.radius);
+// console.lsog(c.#radius);
