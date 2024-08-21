e6962ac1d68e5fa9d9cdbf28febcc3c27e0ff0be
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-21 18:38:42 +0330

    static methods

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 class Triangle {
   constructor(a, b) {
-    if (a <= 0 || b <= 0) {
-      throw new Error("you canttt");
-    }
     this.a = a;
     this.b = b;
   }
-  getArea() {
-    return (this.a * this.b) / 2;
-  }
 
-  getHypotenuse() {
-    return this.a ** 2 + this.b ** 2;
-  }
   static sideNumber = 3;
+  static func() {
+    console.log(this);
+  }
+  newFunc() {
+    console.log(this);
+  }
 }
 
 const tri = new Triangle(2, 4);
 console.log(tri.sideNumber);
 console.log(Triangle.sideNumber);
+console.log(Triangle.func());
+// this in a static method refers to the class itself
+// but in a regular method refers to the instance.
