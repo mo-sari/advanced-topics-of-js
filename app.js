54f61f3cdf140aa26f1e3136bfdb674b80ab46da
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-21 19:31:14 +0330

    publice fields

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 class Triangle {
+  edgeNum = 3;
+  //   different than static properties like those are tied to class,
+  // but these are public properties which are tied to instances
+  //   can access to it with this on instance
+  //   can just set a property in constructor like this.edgeNum = 3
+  // but this avoides repetition( clean up our constructor)
+
   constructor(a, b) {
     this._a = a;
     this._b = b;
-    // _, indicating that do not mess with this directly
-    // can define a function for validation and use it in
-    // both constructor and setter function
-  }
-  //   getter syntax in js
-  get area() {
-    return (this._a * this._b) / 2;
-  }
-  get a() {
-    return this._a;
-  }
-  set a(value) {
-    // check for value to validate
-    this._a = a;
   }
 }
 
 const tri = new Triangle(3, 4);
-console.log(tri.area);
+console.log(tri.edgeNum);
