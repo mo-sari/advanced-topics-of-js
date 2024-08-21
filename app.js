eaa0e172e6b3c84458de498e2bae4728e78c1aa8
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-21 17:42:55 +0330

    starting point of class declaration

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
-// ====================================
-// Plain old javascript objects (pojo)
-const pet = {};
-console.log(typeof pet);
-
-const newPet = { species: "dog", name: "elton" };
-console.log(newPet.species);
-console.log(newPet["name"]);
-
-const key = "species";
-console.log(newPet.key);
-// above looks for the exact key keyword, below evaluates it to a string first.
-console.log(newPet[key]);
-// not existing variable evaluate to undefined
-// all key's get stringified
-// pet[true] == pet['true']
-// propertie values could be functions
-
-pet.bark = function () {
-  return "woof woof";
-};
-
-let tri = {
-  a: 3,
-  b: 4,
-  getArea: function () {
+class Triangle {
+  getArea() {
     return (this.a * this.b) / 2;
-  },
-};
+  }
 
-// we created a complete object and we can go on this way but creating lot's of objects
-// in this way probably is going to be annoying. ( and we also need to repeat the same
-// function's in different objects that wastes lot's of memory space)
+  getHypotenuse() {
+    return this.a ** 2 + this.b ** 2;
+  }
+}
+// this keyword points to the instance not the Triangle class
+
+const triA = new Triangle();
+const triB = new Triangle();
+
+console.log(typeof triA);
+triA.a = 2;
+triA.b = 3;
+
+console.log(triA.getArea());
+// we used class format which still outputs an object, but we can share the functionaltites
+// among as many objects as we want.
+
+console.log(triA instanceof Triangle);
+console.log(typeof triA);
