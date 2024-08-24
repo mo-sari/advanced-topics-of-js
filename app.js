700b9f556c0dbea6deca7789953e648b77169bf0
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-24 16:12:37 +0330

    new section{this}

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-class Circle {
-  #radius;
-  #privateMethod() {
-    console.log("this is private");
-  }
-  constructor(r) {
-    this.#radius = r;
-  }
-  get radius() {
-    this.#privateMethod();
-    return this.#radius;
-  }
-}
-
-const c = new Circle(10);
-// c.#privateMethod();
-c.radius;
+// new section {this keyword}
