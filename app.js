670f5d821693adfd6a6370a27d7c33e94107dcdf
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-24 19:29:13 +0330

    prototype chain

--- app.js
+++ app.js
@@ -1,40 +1,22 @@
-function Dog(name, breed) {
-  this.name = name;
-  this.breed = breed;
-}
-
-const dog1 = new Dog("dog", "breed");
-const dog2 = new Dog("dog2", "breed2");
-
-Dog.prototype.bark = function () {
-  return `${this.name} says woof!`;
+const grandGreet = {
+  greet() {
+    return "hello";
+  },
 };
 
-console.log(dog1, dog2);
-console.log(dog1.__proto__ === dog2.__proto__);
+const parentGreet = {
+  secondGreet() {
+    return "secondHello";
+  },
+  age: 30,
+  __proto__: grandGreet,
+};
 
-// for objects created by factory function's we have to manualy add
-// function's to their prototype
+const child = {
+  num: 2,
+  __proto__: parentGreet,
+};
 
-class NewDog {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
-  }
-
-  bark() {
-    return `${this.name} says woof!`;
-  }
-}
-const dog3 = new NewDog("dog", "breed");
-const dog4 = new NewDog("dog2", "breed2");
-
-console.log(dog3.__proto__);
-
-console.log(dog3.__proto__ === dog4.__proto__);
-// every object has access to another special object called prototype
-// that is created when we use class keyword.
-
-// remember in both cases the new keyword is
-// linking the objects with their respective prototype's
-// like link's dog1 to Dog's prototype
+console.log(child.greet());
+// it goes up the chain to find what we used
+// child now has access to all details of parentGreet and grandGreet
