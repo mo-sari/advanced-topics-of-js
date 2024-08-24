f68b0e094fb4461c3e0593598377623e66345b4b
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-24 16:18:11 +0330

    first example and problem

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// new section {this keyword}
+const person = {
+  name: "ali",
+  hello: function () {
+    return "hello";
+  },
+};
+
+console.log(person.hello());
+const pHello = person.hello;
+console.log(pHello());
+
+const newPerson = {
+  name: "ali",
+  hello: function () {
+    return `${this.name} says hello`;
+  },
+};
+console.log(newPerson.hello());
+const pNewHello = newPerson.hello;
+console.log(pNewHello());
+
+// there is a slight problem here and we'll talk about that in this section
