bfb5cf4aca5d32a8ce3718b480d6ba42e5004fb8
mo-sari <mohsen.ansari.bim@gmail.com>
2024-09-08 10:28:11 +0330

    async await

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-// promises:
-// A promise of one-time guarantee of future value or failure.
+// async
 
-fetch("http://google.com")
-  .then((response1) => {
-    console.log(response1);
-    fetch("http://google.com")
-      .then((response2) => {
-        console.log(response2);
-        fetch("http://google.com")
-          .then((response3) => {
-            console.log(response3);
-          })
-          .catch((err3) => {
-            console.log(err3);
-          });
-      })
-      .catch((err2) => {
-        console.log(err2);
-      });
-  })
-  .catch((err1) => {
-    console.log(err1);
-  });
+// we can declare any function as async, they
+// always return a promise
 
-// still have to chain promises together to have async functionality in order.
+// await
+// inside async function's we can use await, it
+// pauses execution, think of the await keyword like a pause button
 
-// this below is the prefered way, returning a promise after the fetch was completed and chain a .then to it
-fetch("http://google.com")
-  .then((response1) => {
-    console.log(response1);
-    return fetch("http://google.com");
-  })
-  .then((res2) => {
-    console.log(res2);
-    return fetch("http://google.com");
-  })
-  .then((res3) => {
-    console.log(res3);
-    return fetch("http://google.com");
-  });
+async function getData() {
+  const result = await fetch("url");
+  console.log(result);
+}
+// this will wait for the data back and then log it,
+// but the below function will log the promise that was made
+
+function getData2() {
+  const result = fetch("url");
+  console.log(result);
+}
