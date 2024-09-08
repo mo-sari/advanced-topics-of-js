a1ebadb2cdabcc5f32756ebe1ba3165fe41c3fee
mo-sari <mohsen.ansari.bim@gmail.com>
2024-09-08 09:06:32 +0330

    introduction to callbacks

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// new section (async await)
+//callback function's:
+// just functions we pass to other function's to call
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const evens = arr.filter((num) => num % 2 === 0);
+console.log(evens);
+
+// other example's include passing a callback to eventListener's, async operation's
+function newfun() {
+  console.log("hello");
+}
+setTimeout(newfun, 3000);
