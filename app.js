7d926f5cfbaf75995b0ff0f29ce52f2df2db8178
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-22 00:17:35 +0330

    javascript generator"s

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-// there is a difference between x++ and ++x
+// js Generators
 
-let x = 5;
-let y = x++;
+function* evens(n){
+  while(true){
+    yield n;
+    n+=2;
+  }
+}
+const even = evens()
+console.log(even.next());
 
-console.log(x, y);
+function* colorsGen (){
+  yield 'blue';
+  yield 'red';
+  yield 'brown';
+  yield 'yellow';
+}
 
-
-x = 5
-y = ++x;
-console.log(x, y);
+// this generator in js is useful when you have a big chunck of data and want to use a part
+// of it every time
