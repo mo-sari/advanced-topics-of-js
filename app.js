0526bfa56ff3983bf05c4819bfcfe17834ee0cbd
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-21 20:30:53 +0330

    isNaN vs Number.isNaN

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// there is also Nullish coalescing assignment = 
+// isNaN only returns false if it's not NaN and the value can not be interpreted as a number
+console.log(isNaN('sfse'));
+console.log(isNaN([]));
+console.log(Number([]));
 
-let person = null;
-let newPerson = 'ali';
 
-
-person ??= 'firstPersonsName';
-newPerson ??= 'newPersonsName';
-
-console.log(person, newPerson);
+// Number.isNan will return false if the argument is anything but Nan
+console.log(Number.isNaN('sfse'));
+console.log(Number.isNaN([]));
+console.log(Number.isNaN(0/0));
