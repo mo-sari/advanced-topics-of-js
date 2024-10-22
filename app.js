5537dda408e7c0e57f977f2145e07a8bc30d4071
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-22 17:40:01 +0330

    closure in js;

--- app.js
+++ app.js
@@ -1,36 +1,90 @@
-// Hoisting
+// closure
+// closure is a function inside another function which we have returned and this inner
+// function has access to the outter function's scope and it's varialbles even after
+// the execution of the outter function is finished.
 
-console.log(name);
-// this line will throw an error, but below will return undefined
-console.log(lastName);
-var lastName = 'ansari';
-// why? becuase what js does when it see's a var declaration is like this:
-// it first brings the var to the top of the scope
-
-var lastName = undefined;
-console.log(lastName);
-lastName = 'ansari';
-
-// this is the same if we were doing that inside a function as well
-function hoisting(){
-  console.log(newfirstname);
-  var newfirstname = 'mohsen';
-}
-
-hoisting();
-
-// for let and const however this works differently, we get an error if we
-// use the variable before declaring it, js again does hoist the variable
-// to the top of the scope, but puts it in a TEMPORAL DEAD ZONE that we can't reach it
-// (for simplicity you can think of it like js does not HOIST the let and const variables
-// at all)
-
-// an interesting example:
-function blah(){
-  if(false){
-    var message = 'hello'
+function outterfunction(){
+  const variable = 'something';
+  function inner(){
+    console.log(variable);
   }
-  console.log(message);
+  return inner;
 }
-blah();
-// in this example we don't get an error, why do you think that is ?
+
+const closure = outterfunction();
+console.log(closure());
+
+
+function idGenerator(){
+  let count = 1
+  function innerIdGen(){
+    return count++;
+  }
+  return innerIdGen;
+}
+
+// const idGen = idGenerator()
+// for (let i = 0; i < 10; i++) {
+//   console.log(idGen());  
+// }
+
+// =======================================================
+// this concept could be useful in scenario's wher we want to hide a variable
+// (kind of make them private) and force the user to access to it through function's, like:
+
+// function createCounter(){
+//   let count = 0;
+//   return {
+//     increment: function (){
+//       return count++;
+//     },
+//     decrement: function(){
+//       return count--;
+//     },
+//     getCount: function(){
+//       console.log(count);
+//     }
+//   }
+// }
+
+// const counter = createCounter()
+// for (let i = 0; i < 10; i++) {
+//   if(Math.random() > 0.5){
+//     counter.increment()
+//   }else{
+//     counter.decrement()
+//   }
+//   counter.getCount();
+// }
+
+// =============================================================
+// another usecase is to create factory function's
+
+function createExponentFunction(exponent){
+  function value(val){
+    return val** exponent;
+  }
+  return value;
+}
+
+const squre = createExponentFunction(2);
+const cube = createExponentFunction(3);
+
+for (let index = 1; index < 10; index++) {
+  console.log(squre(index));
+  console.log(cube(index));
+  console.log('========================================')
+}
+
+// or another example:
+function uniuqueIdGen(prefix){
+  let id = 0;
+  return function(){
+    id++;
+    return `${prefix}_${id}`;
+  }
+}
+const idgenerator = uniuqueIdGen('book')
+for (let index = 1; index < 10; index++) {
+  console.log(idgenerator());
+}
