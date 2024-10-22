e38fc0a938531f03bbf01c1beef9db16ead2a32b
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-22 13:35:24 +0330

    scopes part1

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-// var 
+// when we declare a variable with let, we define it in global scope
+// and we can reach it using window.<<varName>>, this is not good because we might
+// change something in window object unintentionaly
+// but let and const are also global but are not saved in window object
 
-var person = 'ali'
-// if you declare a global var variable like this you have access to it everywhere
+// in a function if we declare a variable, it doesn't matter if we use let, var or const
+// they're all bined to that function's scope
 
-function newPerson (){
-  var myName = 'mohsen'
-  console.log(myName);
+// another distinction is that let and const are block scope(if, for ...) but var is not
+
+// The scope chain:
+// 1. In the local scope
+// 2. Any outer functions
+// 3. Global scope
+
+// js is static scope
+let animal = 'Barn';
+
+function printAniaml(){
+  console.log(animal);
 }
-// the var in this function is only tied to the function's scop
-// this will throw an error:
-// console.log(myName);
 
-
-// a var variable in any code block like an if or for is scoped to global scope(window)
-if(true){
-  var myLastName = 'ansari';
+function alsoPrintAnimal(){
+  let animal = 'new owl'
+  printAniaml();
 }
 
-console.log(myLastName);
+console.log(alsoPrintAnimal());
+// the scope of printAniaml is determined when we wrote this function, that is why this is happening
