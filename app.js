f75ce8110b22a67e10cbc5505d68c58e4c8b99a8
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-24 17:34:01 +0330

    localStorage part 2

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ JSON.parse(localStorage.getItem('nums'));
 // NOTHING SENSITIVE SHOULD BE STORED IN LOCALSTORAGE LIKE PASSWORD'S ...
 // what we can store in ls ? stuff like user preferences(dark mode ...), the products
 // which user has reviewd, if they have field any form partially we could store that, shopping card data, caching data, analytics, add trackings ...
+
+// sometimes we open the same page in multiple tabs and we might change the localstorage
+// and we might want to keep all the pages in sync, what happens by default is that 
+// localstorage is the same across all the pages but to make sure dome elements stay in
+// sync with localstorage data we have to do :
+
+window.addEventListener('storage',(e)=>{
+  // first make sure the key we're dealing with has changed
+  if(e.key === 'key'){
+    // run the functionality that will update other pages.
+  }
+})
