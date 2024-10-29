e0a6cb8cd505174ddb223a1214f61c27586a520d
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-29 11:54:57 +0330

    IO part 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // new Section: Intersection Observer
 
+// we can add other information abou when this callback function should be triggered, like threshold, if we set it to 1, it means that 100 percent of the target must be in our viewport to run the callback function, if we set it to 0, even the tiniest px of target being on the viewport will trigger the function
 const Observer = new IntersectionObserver(entries =>{
     entries.forEach(entry=>{
         if(entry.isIntersecting){
@@ -7,7 +8,7 @@ const Observer = new IntersectionObserver(entries =>{
             console.log(entry);
         }
     })
-})
+},{threshold: 0});
 
 const box = document.querySelector('.box');
 Observer.observe(box);
