922996aa045bf311e0b5cbdbe9976f8a6e268a07
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-29 11:43:38 +0330

    intersection observer part1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // new Section: Intersection Observer
+
+const Observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            console.log('Yeah I saw it');
+            console.log(entry);
+        }
+    })
+})
+
+const box = document.querySelector('.box');
+Observer.observe(box);
