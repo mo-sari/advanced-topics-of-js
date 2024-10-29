5e77a9cf81eb30f092a9bcd26b72147f950e39db
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-29 12:11:49 +0330

    IO part3

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // new Section: Intersection Observer
 
-// we can add other information abou when this callback function should be triggered, like threshold, if we set it to 1, it means that 100 percent of the target must be in our viewport to run the callback function, if we set it to 0, even the tiniest px of target being on the viewport will trigger the function
-const Observer = new IntersectionObserver(entries =>{
-    entries.forEach(entry=>{
+// if we wanted to do something like tracking ad's viewtime:
+
+let addViewTimes = []
+let addVisibleStartTime;
+
+const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry =>{
         if(entry.isIntersecting){
-            console.log('Yeah I saw it');
-            console.log(entry);
+            addVisibleStartTime = Date.now();
+        }
+        else if(addVisibleStartTime){
+            let addVisibilityDuration = Date.now() - addVisibleStartTime
+            addViewTimes.push(addVisibilityDuration);
+            console.log(addViewTimes);
         }
     })
-},{threshold: 0});
+}, {threshold: 0.5});
 
 const box = document.querySelector('.box');
-Observer.observe(box);
+observer.observe(box);
