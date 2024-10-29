063c0ca416391b2dff1a8b574d8fcb8062b48c9d
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-10-29 15:09:12 +0330

    performance api part final

--- app.js
+++ app.js
@@ -1,47 +1,7 @@
 // new Section: Performance API
 
-// let's create an array and try to sort it with two differenct approaches and take a look at performance with Performance API:
+// this API automaticaly will provide us with resource load times, if we have script's, images or links it will track the performance of them.
 
+console.log(performance.getEntriesByType('resource'))
 
-
-function bubbleSort(arr){
-    let len = arr.length;
-    for(let i =0; i< len; i++){
-        for (let j = 0; j < len - i - 1; j++){
-            if(arr[j] > arr[j+1]){
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    return arr;
-}
-
-// ===========================================================
-const largeArray = Array.from({length: 1000}, ()=>{
-    return Math.floor(Math.random() * 1000);
-});
-
-const arrayForNativeSort = [...largeArray];
-const arrayForBubbleSort= [...largeArray];
-
-// ===========================================================
-performance.mark('nativeStart');
-arrayForBubbleSort.sort((a, b) => a-b);
-performance.mark('nativeEnd');
-
-performance.measure('nativePerformance', 'nativeStart', 'nativeEnd');
-// ===========================================================
-
-performance.mark('bubbleStart');
-bubbleSort(arrayForBubbleSort);
-performance.mark('bubbleEnd');
-
-performance.measure('bubblePerformance', 'bubbleStart', 'bubbleEnd');
-// ===========================================================
-
-const nativeDuration = performance.getEntriesByName('nativePerformance')[0].duration;
-const bubbleDuration = performance.getEntriesByName('bubblePerformance')[0].duration;
-
-console.log(nativeDuration, bubbleDuration);
+// search more about performance api
